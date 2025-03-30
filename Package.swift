322a052cf93260e6f64e4f8d1b3df491bca1e61e
rhgrive <68181385+RHgrive@users.noreply.github.com>
2025-03-31 01:07:39 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,32 +1,17 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "Capstone",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Capstone",
-            targets: ["Capstone"]
-        )
-    ],
+    name: "capstone-swift",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Capstone", url: "https://github.com/zydeco/capstone-swift", .branch("v4")),
     ],
     targets: [
-        .systemLibrary(name: "Ccapstone",
-                       pkgConfig: "capstone",
-                       providers: [
-                        .brew(["capstone"])
-        ]),
         .target(
-            name: "Capstone",
-            dependencies: ["Ccapstone"]),
-        .testTarget(
-            name: "CapstoneTests",
-            dependencies: ["Capstone"])
+            name: "capstone-swift",
+            dependencies: [
+                "Capstone",
+            ]
+        ),
     ]
 )
