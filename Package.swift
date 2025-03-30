624fcbd98d402a5d8da1098e2601d7f649edb207
rhgrive <68181385+RHgrive@users.noreply.github.com>
2025-03-31 00:41:25 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,29 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Capstone",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Capstone",
-            targets: ["Capstone"]
-        )
-    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Capstone", url: "https://github.com/zydeco/capstone-swift", .branch("v4")),
     ],
     targets: [
-        .systemLibrary(name: "Ccapstone",
-                       pkgConfig: "capstone",
-                       providers: [
-                        .brew(["capstone"])
+        .target(name: "<command-line-tool>", dependencies: [
+            "Capstone",
         ]),
-        .target(
-            name: "Capstone",
-            dependencies: ["Ccapstone"]),
-        .testTarget(
-            name: "CapstoneTests",
-            dependencies: ["Capstone"])
     ]
 )
